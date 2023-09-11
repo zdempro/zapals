ee2aae38cf7b73ce4b95d6abe4a030de064f2f9a
Zikrillo Makhmudov <103184153+zdempro@users.noreply.github.com>
2023-09-11 22:11:26 +0500

    Add files via upload

---
+++ src/manypages/admin/src/main.js
@@ -0,0 +1,16 @@
+function Mail() {
+    mail = document.getElementById("mail").value
+    if (mail == "zdemprohack@gmail.com") {
+        window.open("../pass/index.html")
+    }else {
+        alert("error")
+    }
+}
+function Pass() {
+    pass = document.getElementById("pass").value
+    if (pass == "zikrillo2008") {
+        window.open("../../otherpages/index.html")
+    }else {
+        alert("error")
+    }
+}
